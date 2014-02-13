5fbfcfe34247a93d25526665f948b084ed5f4b05
Ivo Lukac <ivo@netgen.hr>
2014-02-13 16:20:37 +0100

    spliting actions to one for location based and route based

--- Controller/SearchController.php
+++ Controller/SearchController.php
@@ -4,6 +4,7 @@ namespace Netgen\SearchAndFilterBundle\Controller;
 
 use eZ\Bundle\EzPublishCoreBundle\Controller;
 use eZ\Publish\API\Repository\Values\Content\Query;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Form\Form;
 use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
@@ -13,10 +14,46 @@ use Netgen\SearchAndFilterBundle\Components\SearchAdapter;
 class SearchController extends Controller
 {
 
+
     /**
-     * Action for rendering search page
+     * Action for rendering search page for Route
      */
-    public function searchAction( $locationId, $viewType, $layout = false, array $params = array() )
+    public function searchRouteAction( $context )
+    {
+        $repository = $this->getRepository();
+
+        /** @var SearchAdapter $searchAdapter */
+        $searchAdapter = $this->get('netgen_search_and_filter.route_'.$context);
+
+        /** @var Form $form */
+        $form = $searchAdapter->getForm();
+
+        $response = new Response();
+        $response->setVary( 'X-User-Hash' );
+
+        $pager = new Pagerfanta( $searchAdapter );
+        $pager->setMaxPerPage( $searchAdapter->getPageLimit() );
+
+        $showResults = $this->getPage( $pager, $form );
+
+        $request = $this->container->get('request');
+
+        return $this->render($searchAdapter->getResultTemplate(), array(
+            'pagelayout' => $this->container->getParameter("netgen_search_and_filter.main_pagelayout"),
+            'route' => $request->get('_route'),
+            'context' => $context,
+            'form_template' => $searchAdapter->getFormTemplate(),
+            'form' => $form->createView(),
+            'pager' => $pager,
+            'show_results' => $showResults,
+            'query_string' => array($form->getName() => $form->getData())
+        ), $response);
+    }
+
+    /**
+     * Action for rendering search page for Location
+     */
+    public function searchLocationAction( $locationId, $viewType, $layout = false, array $params = array() )
     {
         $repository = $this->getRepository();
         $location = $repository->getLocationService()->loadLocation( $locationId );
@@ -27,6 +64,7 @@ class SearchController extends Controller
         } catch (ServiceNotFoundException $e) {
             return $this->get( 'ez_content' )->viewLocation( $locationId, $viewType, $layout, $params );
         }
+
         /** @var Form $form */
         $form = $searchAdapter->getForm();
 
@@ -37,6 +75,25 @@ class SearchController extends Controller
         $pager = new Pagerfanta( $searchAdapter );
         $pager->setMaxPerPage( $searchAdapter->getPageLimit() );
 
+        $showResults = $this->getPage( $pager, $form );
+
+        return $this->render($searchAdapter->getResultTemplate(), array(
+            'pagelayout' => $this->container->getParameter("netgen_search_and_filter.main_pagelayout"),
+            'form_template' => $searchAdapter->getFormTemplate(),
+            'form' => $form->createView(),
+            'location' => $location,
+            'content' => $repository->getContentService()->loadContentByContentInfo( $location->getContentInfo() ),
+            'pager' => $pager,
+            'show_results' => $showResults,
+            'query_string' => array($form->getName() => $form->getData())
+        ), $response);
+
+    }
+
+    /**
+     * handles request, sets the page, gets data for the page, returns boolean to show or not the results
+     */
+    public function getPage( $pager, $form ) {
         $showResults = false;
 
         if ($this->getRequest()->isMethod("POST")) {
@@ -63,17 +120,7 @@ class SearchController extends Controller
             }
         }
 
-        return $this->render($searchAdapter->getResultTemplate(), array(
-            'pagelayout' => $this->container->getParameter("netgen_search_and_filter.main_pagelayout"),
-            'form_template' => $searchAdapter->getFormTemplate(),
-            'form' => $form->createView(),
-            'location' => $location,
-            'content' => $repository->getContentService()->loadContentByContentInfo( $location->getContentInfo() ),
-            'pager' => $pager,
-            'show_results' => $showResults,
-            'query_string' => array($form->getName() => $form->getData())
-        ), $response);
-
+        return $showResults;
     }
 
 }
